0d817f660cc6b2e2b15c5dc283e75e680c537056
yps <afei@email.com>
2020-10-20 11:01:48 +0800

    页面动态初始化

--- js/amountCalculation.js
+++ js/amountCalculation.js
@@ -1,4 +1,30 @@
 $(document).ready(() => {
+    let yuan = [10000, 5000, 1000, 100, 50, 20, 10, 5, 1],
+        jiao = [5, 1],
+        fen = [5, 1];
+    function initForm() {
+        yuan.forEach((item) => {
+            $('.form form').append(`<div class="item">
+            <span>${item}元:</span>
+            <input type="number" attr-num="${item}">
+            <button class="reast">重置</button>
+            </div>`);
+        });
+        jiao.forEach((item) => {
+            $('.form form').append(`<div class="item">
+            <span>${item}角:</span>
+            <input type="number" attr-num="${item/10}">
+            <button class="reast">重置</button>
+            </div>`);
+        });
+        fen.forEach((item) => {
+            $('.form form').append(`<div class="item">
+            <span>${item}分:</span>
+            <input type="number" attr-num="${item/100}">
+            <button class="reast">重置</button>
+            </div>`);
+        });
+    }
     function reastAllNum() {
         $('.form .item').each(function () {
             $(this).find('input').val(0);
@@ -14,9 +40,11 @@ $(document).ready(() => {
             $(this).val(0);
         }
     });
+    // 单个输入框输入重置
     $('.reast').on('click', function () {
         $(this).prev().val(0);
     });
+    // 计算一次上面输入结果
     $('.submit').on('click', function () {
         let num = 0;
         $('.form .item').each(function () {
@@ -34,13 +62,16 @@ $(document).ready(() => {
         </p>`);
         $('.result').text(num);
     });
+    // 清空上面输入框内容
     $('.reast-all').on('click', function () {
         reastAllNum();
     });
+    // 清空页面所有内容
     $('.clear-all').on('click', function () {
         reastAllNum();
         $('.outPut .inner').html('');
     });
+    // 计算额外加加减减后的结果
     $('.compute-finally').on('click', function () {
         $('.outPut .inner p').each(function () {
             let first = parseFloat($(this).find('.first').text()),
@@ -54,5 +85,6 @@ $(document).ready(() => {
             $(this).find('input').addClass('hide');
         });
     });
+    initForm();
     reastAllNum();
 })
